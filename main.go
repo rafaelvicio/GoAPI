9278f4c71fc1261cc4c5311b706137db58b3adf7
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-16 17:16:41 -0300

    Simple routes

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
 
 	log.Fatal(http.ListenAndServe(":8180", router))
 
@@ -19,4 +21,14 @@ func main() {
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintf(w, "Todo Show:", todoId)
 }
